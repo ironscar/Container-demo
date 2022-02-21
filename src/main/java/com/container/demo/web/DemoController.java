6bca4c434dd7e7b1888017a16cd9c07496548fc9
ironscar <uchakravarty29@gmail.com>
2022-02-21 12:25:49 +0530

    fix: fix previous intentional break

--- src/main/java/com/container/demo/web/DemoController.java
+++ src/main/java/com/container/demo/web/DemoController.java
@@ -18,7 +18,7 @@ public class DemoController {
         map.put("key1", "value1 " + new Date().toString());
         map.put("key2", "value2 " + new Date().toString());
         map.put("key3", "value3 " + new Date().toString());
-        return map.toSplendid();
+        return map;
     }
     
 }
